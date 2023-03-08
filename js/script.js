709ccf553ba02ad9e8c96a8fb36a4809f484e1f2
Valentin <valentin.mudriak@gmail.com>
2023-03-08 15:28:32 +0200

    timer adjustment

--- js/script.js
+++ js/script.js
@@ -40,15 +40,24 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     // Timer
-    const deadLine = '2023-03-09';
+    const deadLine = '2023-02-09';
 
     function getTimeRemaining(endTime) {
-        const t = Date.parse(endTime) - Date.parse(new Date()),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+            
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
             hours = Math.floor((t / (1000 * 60 * 60) % 24)),
             minutes = Math.floor((t / (1000 * 60) % 60)),
             seconds = Math.floor((t / 1000) % 60);
-        
+        }
+          
         return {
         'total': t,
         'days': days,
@@ -59,7 +68,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function getZero(num) {
-        if (num > 0 && num < 10) {
+        if (num => 0 && num < 10) {
             return `0${num}`;
         } else {
             return num;
