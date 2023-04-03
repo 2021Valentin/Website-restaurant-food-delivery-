5186632b06fe32e6cbcdb56154826258dc8f6faa
Valentin <valentin.mudriak@gmail.com>
2023-04-03 20:50:16 +0300

    Added additional fanctions

--- js/script.js
+++ js/script.js
@@ -315,7 +315,28 @@ window.addEventListener('DOMContentLoaded', () => {
           slides = document.querySelectorAll('.offer__slide'),
           slidesWrapper = document.querySelector('.offer__slider-wrapper'),
           slidesField = document.querySelector('.offer__slider-inner'),
-          width = window.getComputedStyle(slidesWrapper).width;
+        width = window.getComputedStyle(slidesWrapper).width;
+    
+    function illuminationPoints(dots) {
+        dots.forEach(item => item.style.opacity = '0.5');
+        dots[slideIndex - 1].style.opacity = '1';
+    }
+     
+    function getCurrentNumber(slide) {
+        if (slides.length < 10) {
+            slide.textContent = `0${slideIndex}`;
+        } else {
+            slide.textContent = slideIndex;
+        }
+    }
+
+    function moveSlides(item) {
+        item.style.transform = `translateX(-${offset}px)`;
+    }
+    
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
 
     // Slider 1
     let slideIndex = 1;
@@ -361,13 +382,13 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     sliderNext.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`;
+        moveSlides(slidesField);
 
         if (slideIndex == slides.length) {
             slideIndex = 1;
@@ -375,24 +396,19 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            currentSlides.textContent = `0${slideIndex}`;
-        } else {
-            currentSlides.textContent = slideIndex;
-        }
+        getCurrentNumber(currentSlides);
 
-        dots.forEach(item => item.style.opacity = '0.5');
-        dots[slideIndex - 1].style.opacity = '1';
+        illuminationPoints(dots);
     });
 
     sliderPrev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`;
+        moveSlides(slidesField);
 
         if ( slideIndex == 1) {
             slideIndex = slides.length;
@@ -400,40 +416,32 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        if (slides.length < 10) {
-            currentSlides.textContent = `0${slideIndex}`;
-        } else {
-            currentSlides.textContent = slideIndex;
-        }
+        getCurrentNumber(currentSlides);
 
-        dots.forEach(item => item.style.opacity = '0.5');
-        dots[slideIndex - 1].style.opacity = '1';
+        illuminationPoints(dots);
     });
 
     dots.forEach(dot => {
         dot.addEventListener('click', (e) => {
             const slideTo = e.target.getAttribute('data-slide-to');
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
-            slidesField.style.transform = `translateX(-${offset}px)`;
+            offset = deleteNotDigits(width) * (slideTo - 1);
+            moveSlides(slidesField);
 
-        if (slides.length < 10) {
-            currentSlides.textContent = `0${slideIndex}`;
-        } else {
-            currentSlides.textContent = slideIndex;
-        }
+        getCurrentNumber(currentSlides);
 
-        dots.forEach(item => item.style.opacity = '0.5');
-        dots[slideIndex - 1].style.opacity = '1';
+        illuminationPoints(dots);
         });
     });
+    
+    
+
+    
 
 
 
-    
-    
-   
-    
-    
+
+
+
 
 });
